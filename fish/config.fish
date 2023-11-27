732bd739fa951e04f6bd3fe4f376d1c499e66dbc
niozow <niozow@gmail.com>
2023-11-27 02:20:30 -0600

    Added fish config files

---
+++ fish/config.fish
@@ -0,0 +1,34 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+function fish_prompt
+  
+  # Define colors
+  set color_cyan (set_color cyan)
+  set color_reset (set_color normal)
+  set color_blue (set_color blue)
+  set color_green (set_color green)
+
+  # Convert anything like /home/noah/Code to ~/Code
+  set -g directory (echo $PWD |awk "{ gsub(\"$HOME\", \"~\"); print }")
+
+  # Git repo
+  set git_branch (git branch -a 2>/dev/null|head -n 1 |sed "s/* //g")
+
+  # Create the prompt
+  if [ $status -eq 0 ]
+    echo "$color_cyan"$USER"$color_reset"@"$color_blue"$hostname"$color_reset":$directory "(""$color_green"$git_branch"$color_reset"")"
+  else
+    echo "$color_cyan"$USER"$color_reset"@"$color_blue"$hostname"$color_reset":$directory
+  end
+
+  # Add a newline and the prompt symbol
+  echo -n -s -e "\$ "
+
+end
+
+alias clip="xclip -selection clipboard"
+alias c="xclip -selection clipboard"

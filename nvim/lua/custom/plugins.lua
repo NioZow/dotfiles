f81946dc722f62e4fd42bb47ca5f8000e38f2776
NioZow <niozow@gmail.com>
2023-12-29 00:02:06 +0100

    nvim update

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,28 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "clangd",
+        "pyright",
+        "clang-format",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+return plugins

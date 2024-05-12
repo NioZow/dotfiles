4b045c31de2430b8d1ebbf488060a8955ba40313
NioZow <niozow@proton.me>
2024-05-12 13:58:39 +0200

    nix update

--- scripts/change_alacritty_font.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$1" == "Cascadia" ]]; then
-    cp ~/.config/alacritty/alacritty_cascadia.yml ~/.config/alacritty/alacritty.yml
-elif [[ "$1" == "JetBrains" ]]; then
-    cp ~/.config/alacritty/alacritty_jetbrains.yml ~/.config/alacritty/alacritty.yml
-fi
-

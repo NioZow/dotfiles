6841b02423fb91e18e935658661a41409047501a
noah <noah@deathstar>
2024-06-24 09:36:35 +0200

    update fish && tmux

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
 end
 
 set fish_greeting
+alias ls='eza'
 
 function fish_prompt
   
@@ -18,21 +19,17 @@ function fish_prompt
   # Git repo
   set git_branch (git branch -a 2>/dev/null|grep "*" |sed "s/* //g")
 
-  #set nix_shell (echo $IN_NIX_SHELL)
-  if set -q IN_NIX_SHELL
-    set nix_shell "($color_blue""nixshell""$color_reset) "
-  else
-    set nix_shell ""
-  end
-
   # Create the prompt
   if [ $status -eq 0 ]
-    echo "$nix_shell""$color_cyan"$USER"$color_reset"@"$color_blue"$hostname"$color_reset":$directory "(""$color_green"$git_branch"$color_reset"")"
+    echo "$color_cyan"$USER"$color_reset"@"$color_blue"$hostname"$color_reset":$directory "(""$color_green"$git_branch"$color_reset"")"
   else
-    echo "$nix_shell""$color_cyan"$USER"$color_reset"@"$color_blue"$hostname"$color_reset":$directory
+    echo "$color_cyan"$USER"$color_reset"@"$color_blue"$hostname"$color_reset":$directory
   end
 
   # Add a newline and the prompt symbol
   echo -n -s -e "\$ "
 
 end
+
+# Created by `pipx` on 2024-06-21 10:02:35
+set PATH $PATH /home/noah/.local/bin

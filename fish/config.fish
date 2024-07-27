8a519a583ecbb88b9e3c7b07d08d3d04dee7888c
niozow <niozow@proton.me>
2024-07-27 09:42:01 +0200

    alias enable/disable alsr

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,8 @@ end
 set fish_greeting
 alias ls='eza'
 alias vim='nvim'
+alias aslr_off='echo 0 | sudo tee /proc/sys/kernel/randomize_va_space'
+alias aslr_on='echo 1 | sudo tee /proc/sys/kernel/randomize_va_space'
 
 function fish_prompt
   
